1dff527dbc75a76508262ebf184510a8263f6cfc
German Gordon <german.g@hyros.com>
2026-04-08 11:37:29 -0300

    chore: remove node_modules and dist from tracking

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Register all AI matchers with Playwright's expect()
-require("./matchers");
-//# sourceMappingURL=index.js.map
